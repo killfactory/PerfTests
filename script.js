65b1f2112e40aa3574071acc707be9dc61b95403
Dmitry Kalinin <killfactory@yandex.ru>
2018-04-01 15:34:47 +0300

    Implemented div and table grids for 100k objects. Disable table grid. Calculate time needed to render the objects.

--- script.js
+++ script.js
@@ -1,4 +1,66 @@
-let renderPoint = document.getElementById("renderpoint");
+function getRandomizedDivGrid(width, height) {
+    return getEnclosedHtml(height, (x, i) => getRandomizedDivRow(width), '<div class="grid">', '</div>');
+}
+
+function getRandomizedDivRow(width) {
+    return getEnclosedHtml(width, (x, i) => getRandomizedSpanCell(), '<div>', '</div>');
+}
+
+function getRandomizedSpanCell() {
+    let isBlack = Math.random() >= 0.5;
+    return isBlack ? '<span class="black"></span>' : '<span></span>';
+}
+
+function getRandomizedTable(width, height) {
+    return getEnclosedHtml(height, (x, i) => getRandomizedRow(width), '<table class="grid">', '</table>');
+}
+
+function getRandomizedRow(width) {
+    return getEnclosedHtml(width, (x, i) => getRandomizedCell(), '<tr>', '</tr>');
+}
+
+function getRandomizedCell() {
+    let isBlack = Math.random() >= 0.5;
+    return isBlack ? '<td class="black" />' : '<td />';
+}
+
+function getEnclosedHtml(len, func, openTag, closeTag) {
+    let array = Array.from({length: len + 2}).map((x, i) => i == 0 
+                                                              ? openTag 
+                                                              : i == len + 1 
+                                                                  ? closeTag 
+                                                                  : func(x, i - 1))
+    return array.join('');
+}
+
+function renderSomething(target, func, name){
+    let start = new Date()
+    let something = func();
+    let endCalc = new Date()
+    console.log("calculating " + name + ": " + (endCalc - start));
+    target.innerHTML = something;
+    let endSet = new Date();
+    console.log("set " + name + ": " + (endSet - endCalc));
+    setTimeout(() => {
+    let endRender = new Date();
+    console.log("render " + name + ": " + (endRender - endSet));
+    }, 0);
+}
+
+let renderPoint = document.getElementById('renderpoint');
+let renderPoint2 = document.getElementById('renderpoint2');
+renderSomething(renderPoint2, () => 'Loading......', 'reflow cost for empty page')
 setTimeout(() => {
-    renderPoint.innerHTML = "<b>IS LOADED!</b>";
-}, 1000);
+    renderSomething(renderPoint2, () => getRandomizedDivGrid(400, 250), 'divGrid')
+    setTimeout(() => {
+        renderSomething(renderPoint2, () => getRandomizedDivGrid(400, 250), 'divGrid2')
+        setTimeout(() => {
+            renderSomething(renderPoint2, () => getRandomizedDivGrid(400, 250), 'divGrid3')
+            setTimeout(() => {
+                //renderSomething(renderPoint, () => getRandomizedTable(400, 250), 'table')
+                // this should show the reflow cost
+                renderSomething(renderPoint, () => '', 'reflow cost')
+            }, 0);
+        }, 0);
+    }, 0);
+}, 0);
